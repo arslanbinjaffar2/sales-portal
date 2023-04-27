97ab3e545d5bd2a9d37900cbc15fa06aa3b528b2
Zahid Hussain <zhu@eventbuizz.com>
2023-04-27 19:05:55 +0500

    After password request UI content update

--- src/app/auth/password/request/page.tsx
+++ src/app/auth/password/request/page.tsx
@@ -21,14 +21,14 @@ export default function Login() {
                   <div className="left-signup">
                     <Image src={require('@/app/assets/img/logo.svg')} alt="" width="200" height="29" className='logos' />
                     <div className="text-block">
-                      <h4>WELCOME TO PLUG’N’PLAY</h4>
-                      <p>Minimize your efforts. Maximize the results.</p>
-                      <ul>
-                        <li>Create your own event in a few clicks</li>
-                        <li>Sort out event registration in no time</li>
-                        <li>Get your own customized event app</li>
-                        <li>Feel safe with our step by step navigation</li>
-                      </ul>
+                        <h4>WELCOME TO SALES PORTAL</h4>
+                        <p>Maximize your sales potential with our customizable portal solutions</p>
+                        <ul>
+                            <li>Customization following easy steps</li>
+                            <li>Sales forecasting and analytics</li>
+                            <li>Sort out event registration in no time</li>
+                            <li>Feel safe with our step by step navigation</li>
+                        </ul>
                     </div>
                     <Image src={Illustration} alt="" width="300" height="220" className='illustration' />
                   </div>
