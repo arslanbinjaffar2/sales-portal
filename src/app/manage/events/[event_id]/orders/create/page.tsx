faf38c940fd338c5edd5d3701fb10af44df94fc0
ahmad13054 <mah@eventbuizz.com>
2023-06-23 22:03:29 +0500

    master

--- src/app/manage/events/[event_id]/orders/create/page.tsx
+++ src/app/manage/events/[event_id]/orders/create/page.tsx
@@ -2,15 +2,17 @@
 import { useAppSelector } from '@/redux/hooks/hooks';
 import { RootState } from '@/redux/store/store';
 import Image from 'next/image'
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 export default function page({ params }: { params: { event_id: string } }) {
   const {loading, event, event_orders} = useAppSelector((state: RootState) => state.event);
   const {user} = useAppSelector((state: RootState) => state.authUser);
+  const router = useRouter();
   useEffect(() => {
     const listener = (event:any) =>{
         if(event.data.order_id !== undefined) {
-   
+            router.push(`/manage/events/${params.event_id}/orders`);
         } 
     }
     window.addEventListener("message", listener);
