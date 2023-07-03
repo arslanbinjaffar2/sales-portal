f3542610bfd0e69032d63db177da563a55113e43
ahmad13054 <mah@eventbuizz.com>
2023-07-03 13:52:46 +0500

    master

--- src/app/manage/events/[event_id]/orders/[order_id]/edit/page.tsx
+++ src/app/manage/events/[event_id]/orders/[order_id]/edit/page.tsx
@@ -2,7 +2,7 @@
 import { useAppSelector } from '@/redux/hooks/hooks';
 import { RootState } from '@/redux/store/store';
 import Image from 'next/image'
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function page({ params }: { params: { event_id: string, order_id:string } }) {
