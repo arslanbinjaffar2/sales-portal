d5a1400b2bbe43a5a893c300e6b3053ef68c97c5
ahmad13054 <mah@eventbuizz.com>
2023-09-12 13:39:41 +0500

    SP-108

--- src/app/[locale]/manage/events/[event_id]/orders/[order_id]/edit/page.tsx
+++ src/app/[locale]/manage/events/[event_id]/orders/[order_id]/edit/page.tsx
@@ -5,14 +5,15 @@ import { RootState } from '@/redux/store/store';
 import axios from 'axios';
 import Image from 'next/image'
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { BASE_URL} from '@/constants/endpoints'
 export default function page({ params }: { params: { locale:string, event_id: string, order_id:string } }) {
   const {loading, event, event_orders} = useAppSelector((state: RootState) => state.event);
   const {user} = useAppSelector((state: RootState) => state.authUser);
   const router = useRouter();
   const [cloneOrderId, setCloneOrderId] = useState<any>(null);
- 
+  const [iframeHeight, setIframeHeight] = useState(window.innerHeight - 280);
+
   useEffect(() => {
     const source = axios.CancelToken.source();
     axios.post(`${BASE_URL}/registration/event/${event['event_url']}/registration/clone-order/${params.order_id}`,{}, {
@@ -44,9 +45,41 @@ export default function page({ params }: { params: { locale:string, event_id: st
       window.removeEventListener('message', listener);
     }
   }, []);
+
+
   return (
     <div>
-        {(window !== undefined && cloneOrderId !== null) && <iframe width="100%" height={window.innerHeight - 280} src={`${process.env.regSiteHost}/${event.event_url}/sale/order-summary/${cloneOrderId}?sale_id=${user.id}`} />}
+        {(window !== undefined && cloneOrderId !== null) && <iframe width="100%" height={iframeHeight} src={`${process.env.regSiteHost}/${event.event_url}/sale/order-summary/${cloneOrderId}?sale_id=${user.id}`} 
+        
+        onLoad={(event:any) => {
+          const { contentWindow } = event.target;
+          const main = contentWindow.document.body.querySelector('main');
+
+          // Because the login form has a dynamic height, observe any size changes and update the iframe height
+          const resizeObserver = new ResizeObserver((entries) => {
+            entries.forEach((entry) => {
+              setIframeHeight(entry.contentRect.height);
+            });
+          });
+
+          resizeObserver.observe(main);
+
+          // When the iframe is hiden (i.e. modal is closed), remove any listeners
+          const onVisibilityChange = () => {
+            resizeObserver.disconnect();
+            contentWindow.addEventListener(
+              'visibilitychange',
+              onVisibilityChange
+            );
+          };
+
+          // Add listener for when iframe is hiden (i.e. modal is closed)
+          contentWindow.addEventListener(
+            'visibilitychange',
+            onVisibilityChange
+          );
+        }}
+        />}
     </div>
   )
 }
