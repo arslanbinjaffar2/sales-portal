7478aa2b75c8e29260aac4922838a7145117e9b7
ahmad13054 <mah@eventbuizz.com>
2023-10-02 11:35:28 +0500

    SP-122

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -27,3 +27,12 @@ export const getSelectedLabel = (item, id) => {
       return (obj ? obj.name : '');
     }
 }
+
+export function getCookieValue(name) 
+{
+    const regex = new RegExp(`(^| )${name}=([^;]+)`)
+    const match = document.cookie.match(regex)
+    if (match) {
+    return match[2]
+    }
+}
